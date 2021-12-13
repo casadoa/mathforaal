c85ac49b32974663144b3f57adf8bcec50d784e8
andrescasado <andres.casado@yahoo.com>
2021-12-13 17:52:56 -0500

    Added some more buttons in html more styling of buttons and some more interactivity of buttons

--- index.js
+++ index.js
@@ -1,18 +1,54 @@
 let table = 6;
 let i = 1;
 let msg = '';
-
+ 
 
 //<script src="/jquery-1.11.0.js"></script>
 
+
 $(function() {
-    let $newOperatorButton = $('button');
-       
+    let $newOperatorButton = $('.math:button');
+    let $minMaxOption = $('#choices');
+    let $minText = $('input#min');
+    let $maxText = $('input#max');
+    let temp = 0;
+
+    console.log($minText);
+    console.log($maxText);
+    $minMaxOption.on('input', function(e){
+        e.preventDefault();
+        let minText = $minText.val();
+        
+        let maxText = $maxText.val();
+        if(minText > maxText){  //make sure min bound is always smaller than max even if min entered is larger than max bound
+            temp = minText;
+            minText = maxText;
+            maxText = temp;
+            
+        }
+        
+        console.log('min ' + minText);
+        console.log('max ' + maxText);
+        if(minText.length > 3){
+            console.log('hi');
+        }
+        
+    });
+
+    let $tens = $('.num:button');
+    //console.log($tens);
     $newOperatorButton.on('click',  function math(){
             let msgOperator = '';
             let expression;
+            let operatorArr = ['+', '-', 'x', '/'];
+            let operatorRand = Math.floor(Math.random() * 4);
             let operator = $(this).attr("value");
              console.log(operator);
+             console.log(operatorArr[operatorRand]);
+            if(operator === 'random'){
+                operator = operatorArr[operatorRand];
+                table = Math.floor(Math.random() * 10 + 1);
+            }
             if(operator === '+'){
                 msgOperator = ' + ';
                 expression = (table + i);
