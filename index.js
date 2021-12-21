535787b6e53424162c180c203a88feb9334b41fe
andrescasado <andres.casado@yahoo.com>
2021-12-20 20:17:10 -0500

    Adding new page so once practice/test is clicked it will show an empty blackboard

--- index.js
+++ index.js
@@ -100,26 +100,48 @@ $(function() {
     var table = 1;
     let i = 1;
     let msg = '';
-    let $newOperatorButton = $('.math:button');
-    let $option = $('#choices');
-    let $minText = $('input#min');
-    let $maxText = $('input#max');
-    let $ownNum = $('input#ownNum');
-    let $arrowButton = $('.arrow:button');
-    let $practiceButton = $('#practice');
-
-    $('.num1:button').hide();
-    
-    //console.log($minText);
-    //console.log($maxText);
-    //console.log($ownNum);
-    //console.log($arrowButton);
-    $practiceButton.on('click', function() {
-        $($arrowButton).hide("slow");
+    let $newOperatorButton = $('.math:button');     //rand, +, -, *, / buttons
+    let $option = $('#choices');                    //rand, min, max, ownNum, practice button
+    let $minText = $('input#min');                  //min input 
+    let $maxText = $('input#max');                  //max input
+    let $ownNum = $('input#ownNum');                //own number input
+    let $arrowButton = $('.arrow:button');          //left and right arrow
+    let $practiceButton = $('#practice');           //practice button
+    $('.num1:button').hide();                       //hide ones buttons since we don't know which tens will be clicked
+    $('#page2').hide();
+    $practiceButton.on('click', function() {        //once practice is clicked a bunch of hides so we can just get an empty blackboard view
+       /* $($arrowButton).hide("slow");
         $('.tens').hide("slow");
         $('.test').hide("slow");
         $('.num1:button').hide("slow");
+        $($ownNum).hide("slow");
+        
+        let correctAnswerArray = [];
+        let correctAnswers = 0;
+        if (i == 1) {
+            $("#blackboard").empty(); 
+        }
+        while(i < 11){
+            //msg = math2('+', table).append('<input id="answer" type="text" name="answer" placeholder="44" size="3" maxlength="5" />');
+            msg +=table + ' + ' + i + ' = ' + '<input id="answer" type="text" name="answer" placeholder="" size="3" maxlength="5" />' + '<br />';
+            i++; 
+        }
+        let answer = $('#answer').val();
+        console.log(answer);
+        $("#blackboard").html(msg);
+*/
+        $('#page1').toggle();
+        $('#operator').toggle();
+        $('#test').toggle();
+        $('#page2').toggle();
+        
+    });
 
+    $('#return').on('click', function(){
+        $('#page1').toggle();
+        $('#operator').toggle();
+        $('#test').toggle();
+        $('#page2').toggle();
     });
     $arrowButton.on('click', function(e){
         e.preventDefault();
@@ -133,12 +155,14 @@ $(function() {
                 console.log(table);
                 $leftRight = $('.before');
                 $('.after').empty().append(table);
+                math2('+', table);
                 break;
             case 'more':
                 table++;
                 console.log(table);
                 $leftRight = $('.after');
                 $('.before').empty().append(table);
+                math2('+', table);
                 break;
         }
         $($leftRight).empty().html(table);
@@ -155,24 +179,32 @@ $(function() {
         let minText = $minText.val();
         
         let maxText = $maxText.val();
-        if(minText > maxText){  //make sure min bound is always smaller than max even if min entered is larger than max bound
+        if(ownText){
+            table = ownText;
+        }
+        else if(minText > maxText){  //make sure min bound is always smaller than max even if min entered is larger than max bound
             temp = minText;
             minText = maxText;
             maxText = temp;
-            
+            table = Math.floor(Math.random() * (maxText - minText) + minText); //This value now becomes table to ensure table is between these two numbers
         }
-        table = ownText;
+        
         console.log('own num ' + ownText);
         console.log('min ' + minText);
         console.log('max ' + maxText);
+        //table = ownText;
         if(minText.length > 3 || maxText.length > 3){
             console.log('hi');
             let a = parseInt(minText);
             //maxText.toPrecision(2);
             console.log('min text ' + a);
         }
-        $('.tens').show("slow");
-        $($arrowButton).show("slow");
+        
+            $('.tens').show("slow");
+            $($ownNum).show("slow");
+            $($arrowButton).show("slow");
+            $($ownNum).show("slow");
+        
         math2('random', table);
     });
 
@@ -251,74 +283,55 @@ $(function() {
         let ten = $(this).attr("value");
         $(this).empty().html(ten);
     });
-
-    $newOperatorButton.on('click',  function math(){
-            let msgOperator = '';
-            let expression;
-            let operatorArr = ['+', '-', 'x', '/'];
-            let operatorRand = Math.floor(Math.random() * 4);
-            let operator = $(this).attr("value");
-            console.log(operator + ' in newoperatorButton ' + table + ' table');
-            if(operator === 'random'){
-                operator = operatorArr[operatorRand];
-                table = Math.floor(Math.random() * 50 + 1);
-            }
-             if(operator === '+'){
-
-        //math2('+', table);
-                /*msgOperator = String.fromCodePoint(0x002B);
-                expression = (+table + +i);
-
-                while(i < 11){
-                    msg += table + msgOperator + i + ' = ' + (+table + +i) + '<br />';
-                    i++; 
-                }*/
-            } else if (operator === '-') {
-                msgOperator = String.fromCodePoint(0x2212);
-                expression = (table - i);
-                while(i < 11){
-                    msg += table + msgOperator + i + ' = ' + (table - i) + '<br />';
-                    i++; 
-                }
-            } else if (operator === 'x') {
-                msgOperator = String.fromCodePoint(0x00D7);
-                expression = (table * i);
-                while(i < 11){
-                    msg += table + msgOperator + i + ' = ' + (table * i) + '<br />';
-                    i++; 
-                }
-            } else if (operator === '/') {
-                msgOperator = String.fromCodePoint(0x00F7);
+    $newOperatorButton.on('click',  function(){
+        $('.tens').show("slow");
+        $($ownNum).show("slow");
+        $($arrowButton).show("slow");
+        $($ownNum).show("slow");
         
-                while(i < 11){
-                    if(table > 9){
-                        if(i < table){
-                            let remainder = table % i;
-                            if(remainder === 0){
-                                msg += table + msgOperator + i + ' = ' + (table / i).toPrecision(2) + '<br />';
-                                
-                            } else {
-                                msg += table + msgOperator + i + ' = ' + Math.floor((table / i).toPrecision(2)) + ' remainder ' + remainder + '<br />';
-                           }
-                            i++;
-                        } else {
-                            let remainder = i / table;
-                            if(remainder === 0){
-                                msg += table + msgOperator + i + ' = ' + (table / i).toPrecision(1) + '<br />';
-                            } else {
-                                msg += table + msgOperator + i + ' = ' + (table / i).toPrecision(2) + '<br />';
-                           }
-                            i++; 
-                        }
-                    } else {
-                        if(i < table){
+        let msgOperator = '';
+        let expression;
+        let operatorArr = ['+', '-', 'x', '/'];
+        let operatorRand = Math.floor(Math.random() * 4);
+        let operator = $(this).attr("value");
+        console.log(operator + ' in newoperatorButton ' + table + ' table');
+        if(operator === 'random'){
+            operator = operatorArr[operatorRand];
+            table = Math.floor(Math.random() * 50 + 1);
+        }
+        if(operator === '+'){
+        //math2('+', table);
+            msgOperator = String.fromCodePoint(0x002B);
+            expression = (+table + +i);
+            while(i < 11){
+                msg += table + msgOperator + i + ' = ' + (+table + +i) + '<br />';
+                i++; 
+            }
+        } else if (operator === '-') {
+            msgOperator = String.fromCodePoint(0x2212);
+            expression = (table - i);
+            while(i < 11){
+                msg += table + msgOperator + i + ' = ' + (table - i) + '<br />';
+                i++; 
+            }
+        } else if (operator === 'x') {
+            msgOperator = String.fromCodePoint(0x00D7);
+            expression = (table * i);
+            while(i < 11){
+                msg += table + msgOperator + i + ' = ' + (table * i) + '<br />';
+                i++; 
+            }
+        } else if (operator === '/') {
+            msgOperator = String.fromCodePoint(0x00F7);       
+            while(i < 11){
+                if(table > 9){
+                    if(i < table){
                         let remainder = table % i;
                         if(remainder === 0){
-                            msg += table + msgOperator + i + ' = ' + (table / i).toPrecision(1) + '<br />';
-                            
+                            msg += table + msgOperator + i + ' = ' + (table / i).toPrecision(2) + '<br />';       
                         } else {
                             msg += table + msgOperator + i + ' = ' + Math.floor((table / i).toPrecision(2)) + ' remainder ' + remainder + '<br />';
-                       }
+                        }
                         i++;
                     } else {
                         let remainder = i / table;
@@ -326,21 +339,36 @@ $(function() {
                             msg += table + msgOperator + i + ' = ' + (table / i).toPrecision(1) + '<br />';
                         } else {
                             msg += table + msgOperator + i + ' = ' + (table / i).toPrecision(2) + '<br />';
-                       }
+                        }
                         i++; 
+                        }
+                } else {
+                    if(i < table){
+                    let remainder = table % i;
+                    if(remainder === 0){
+                        msg += table + msgOperator + i + ' = ' + (table / i).toPrecision(1) + '<br />';                
+                    } else {
+                        msg += table + msgOperator + i + ' = ' + Math.floor((table / i).toPrecision(2)) + ' remainder ' + remainder + '<br />';
                     }
-                    }   
+                    i++;
+                    } else {
+                        let remainder = i / table;
+                        if(remainder === 0){
+                            msg += table + msgOperator + i + ' = ' + (table / i).toPrecision(1) + '<br />';
+                        } else {
+                            msg += table + msgOperator + i + ' = ' + (table / i).toPrecision(2) + '<br />';
+                        }
+                        i++;
+                    }
                 }   
-            }
-            //var el = document.getElementById('blackboard');
-            //el.innerHTML = msg;
-        $("#blackboard").html(msg); //jquery equivalent of above expression
-     
+            }   
+        }
+        $("#blackboard").html(msg); 
         if (i == 11) {
             i = 1;
             msg = "";
         }
-        }
-    );
+    });
 });
-
+//var el = document.getElementById('blackboard');
+            //el.innerHTML = msg;
